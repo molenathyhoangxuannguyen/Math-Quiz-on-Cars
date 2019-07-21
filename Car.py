97e5b8020cabb11aab519b7b047dd45d80b7244e
Thy H. Nguyen <46432072+nguyent2@users.noreply.github.com>
2019-07-21 14:24:01 -0400

    Update Car.py

--- Car.py
+++ Car.py
@@ -5,7 +5,7 @@ def main():
         bichthuy.shape("circle")
         bichthuy.shapesize(0.1,0.1)
         bichthuy.speed(10)
-        bichthuy.pensize(4)
+        bichthuy.pensize(5)
         def ve_hinh_tron(number,cao):
             for i in range(number):
                 bichthuy.right(1)
@@ -28,11 +28,11 @@ def main():
         bichthuy.left(90)
         bichthuy.forward(95)
 
-    wns.bgcolor("#33ffff")
+    wns.bgcolor("#1aff8c")
 
     def annyong(mauu):
         ngoc = turtle.Turtle()
-        ngoc.color("#1affff")
+        ngoc.color("#00e673")
         ngoc.penup()
         ngoc.forward(mauu)
         ngoc.pendown()
@@ -40,35 +40,39 @@ def main():
         banh_xe(ngoc)
         ngoc.end_fill()
 
-    annyong(95)
-    annyong(95*2)
-    annyong(95*3)
-    annyong(95*4)
-
+    annyong(-2*150)
+    annyong(-1*150)
+    annyong(0*150)
+    annyong(1*150)
 
     thy = turtle.Turtle()
-    thy.color("#006666")
+    thy.color("#00cc66")
+    thy.penup()
+    thy.forward(-3*150)
+    thy.pendown()
     thy.begin_fill()
     banh_xe(thy)
     thy.end_fill()
 
     thuy = turtle.Turtle()
-    thuy.color("#006666")
+    thuy.color("#00cc66")
     thuy.penup()
-    thuy.forward(95*5)
+    thuy.forward(2*150)
     thuy.pendown()
     thuy.begin_fill()
     banh_xe(thuy)
     thuy.end_fill()
 
     jordan = turtle.Turtle()
+    jordan.color("#00b359")
+    jordan.pensize(6)
     jordan.penup()
     jordan.left(90)
-    jordan.forward(100)
+    jordan.forward(150)
     jordan.right(90)
-    jordan.forward(100)
+    jordan.forward(-150)
     jordan.pendown()
-    jordan.write("Can you see the movement of the car?", move=False, align="center", font =("TimesNewRoman",12,"bold"))
+    jordan.write("Can you see the movement of the car?", move=False, align="center", font =("TimesNewRoman",40,"bold"))
     wns.exitonclick()
 
 main()
